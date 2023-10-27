515565e42189964d95d3d7658ada95fff6e5c540
alessandrofranco189 <alessandrofranco189@gmail.com>
2023-10-27 13:52:53 +0200

    logica di gioco fatta

--- script.js
+++ script.js
@@ -2,4 +2,13 @@
 
 //Recuperare dalla pagina tutti gli elementi di interesse
 const scoreCounter = document.querySelector('.score-counter');
-console.log(scoreCounter);
+const grid = document.querySelector('.grid');
+const endGameScreen = document.querySelector('.end-game-screen');
+const endGameText = document.querySelector('.end-game-text');
+const playAgainButton = document.querySelector('.play-again');
+
+
+//preparo informazioni utili alla logica di gioco
+const toatalCells = 100;
+const totalBombs = 16;
+const maxScore = toatalCells - totalBombs;
