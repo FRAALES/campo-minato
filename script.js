26a830f6e3bec66b6c918288e1161fae20cc0aa0
alessandrofranco189 <alessandrofranco189@gmail.com>
2023-11-02 15:18:05 +0100

    campo a colori alterni

--- script.js
+++ script.js
@@ -12,3 +12,43 @@ const playAgainButton = document.querySelector('.play-again');
 const toatalCells = 100;
 const totalBombs = 16;
 const maxScore = toatalCells - totalBombs;
+const bombsList = []; //Questo è un array vuoto
+let score = 0;
+
+
+//Generare TOT bombe casuali
+while(bombsList.length < totalBombs){
+    const number = Math.floor(Math.random() * toatalCells) + 1;
+    if(!bombsList.includes(number)){
+        bombsList.push(number);
+    }
+};
+
+console.log(bombsList);
+
+
+
+//Griglia e logica di gioco
+
+let isCellEven = false;
+let isRowEven = false;
+
+for(let i=1; 1<=100; i++) {
+    //creo un elemento e gli dò la classe 'cell'
+    const cell = document.createElement('div');
+    cell.classList.add('cell');
+
+    isCellEven = i % 2 ===0;
+
+    //Se la riga è pari e la cella è pari, casella grigia
+    //se la riga è dispari e la cella è dispari, casella grigia
+
+    if(isCellEven && isRowEven) cell.classList.add('cell-dark');
+    else if (!isRowEven && !isCellEven) cell.classList.add('cell-dark');
+
+    //Se sono alla fine della riga...
+    if(i%10 === 0) isRowEven = !isRowEven;
+
+    //Lo inserisco nella griglia
+    grid.appendChild(cell);
+};
